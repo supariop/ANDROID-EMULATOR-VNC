1d6fcf9f1fe1f0b9a187d5d586ac85040043a00b
supariop <noobsupari@gmail.com>
2025-11-14 19:52:15 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,19 +1,12 @@
-start.sh:
-
- 
 #!/bin/bash
+export DISPLAY=:1
+Xvfb :1 -screen 0 1280x720x16 &
 
 # Start VNC server
-x11vnc -display :1 -rfbauth /root/.vnc/passwd -forever -shared &
+x11vnc -display :1 -nopw -listen localhost &
 
-# Start emulator
-emulator -avd Pixel_API${ANDROID_VERSION##*.} -no-audio -no-window -verbose -writable-system &
-
-# Wait for emulator to boot
-sleep 60
-
-# Connect to ADB
-adb connect localhost:5555
+# Start Android Emulator
+emulator -avd Pixel_API_28 -no-window -no-audio -no-boot-anim &
 
 # Keep the container running
 tail -f /dev/null
